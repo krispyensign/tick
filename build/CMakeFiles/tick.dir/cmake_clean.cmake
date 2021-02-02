4484dacf576f37d6cb0f95602381c74356f388af
KrispyEnsign <futralj@gmail.com>
2021-02-02 09:27:33 -0500

    got clangd working

---
+++ build/CMakeFiles/tick.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tick.dir/src/main.cpp.o"
+  "bin/tick"
+  "bin/tick.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
