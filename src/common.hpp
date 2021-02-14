a16d35eb0bbc52d117c0b65ca73dc4cf2be8a10c
KrispyEnsign <futralj@gmail.com>
2021-02-14 11:05:13 -0500

    added more changes

--- src/common.hpp
+++ src/common.hpp
@@ -3,21 +3,10 @@
 
 #pragma region includes
 #include <algorithm>
-#include <iterator>
 #include <string>
-#include <exception>
-#include <stdexcept>
 #include <variant>
 #include <vector>
-#include <algorithm>
 #include <utility>
-
-#include <cpprest/http_client.h>
-#include <cpprest/http_msg.h>
-
-#include <rapidjson/document.h>
-
-#include <fmt/core.h>
 #pragma endregion
 
 #pragma region macros
@@ -32,30 +21,18 @@
 #define let const auto
 #define mut auto
 #define def auto
-#define function auto
-#define begin {
-#define end }
-#define then {
-#define endif }
-#define doloop {
-#define endloop }
+
 #define VA_NUM_ARGS(...) VA_NUM_ARGS_IMPL(__VA_ARGS__, 5,4,3,2,1)
 #define VA_NUM_ARGS_IMPL(_1,_2,_3,_4,_5,N,...) N
-
 #define macro_dispatcher(func, ...) macro_dispatcher_(func, VA_NUM_ARGS(__VA_ARGS__))
 #define macro_dispatcher_(func, nargs) macro_dispatcher__(func, nargs)
 #define macro_dispatcher__(func, nargs) func ## nargs
-
 #define λ(...) macro_dispatcher(λ, __VA_ARGS__)(__VA_ARGS__)
-#define $(...) macro_dispatcher($, __VA_ARGS__)(__VA_ARGS__)
-
 #define λ1(a) [](auto a)
 #define λ2(a, b) [](auto a, auto b)
-#define $1(a) [](auto a)
-#define $2(a, b) [](auto a, auto b)
-
-#define function auto
-#define in :
+#define $(...) macro_dispatcher($, __VA_ARGS__)(__VA_ARGS__)
+#define $1(a) [](const a)
+#define $2(a, b) [](const a, const b)
 
 #define PP_COMMASEQ_N()                                    \
           1,  1,  1,  1,                                   \
@@ -82,7 +59,6 @@
 #define PP_NARG_HELPER3_01(N)    0
 #define PP_NARG_HELPER3_00(N)    1
 #define PP_NARG_HELPER3_11(N)    N
-
 #pragma endregion
 
 #pragma region types
@@ -99,36 +75,36 @@ typedef char* c_str;
 
 #pragma region templates
 template <typename CONTAINER, typename NEW_CONTAINER, typename LAMBDA>
-function fold(CONTAINER container, NEW_CONTAINER new_container, LAMBDA lambda) {
+def fold(CONTAINER container, NEW_CONTAINER new_container, LAMBDA lambda) {
   return accumulate(container.begin(), container.end(), new_container, lambda);
 }
 
 template<typename CONTAINER, typename LAMBDA>
-function maplist(CONTAINER&& container, LAMBDA&& lambda) {
+def maplist(CONTAINER&& container, LAMBDA&& lambda) {
       std::vector<decltype(lambda(*container.begin()))> w;
       std::transform(container.begin(),container.end(),std::back_inserter(w), lambda);
       return w;
 }
 
 template <typename CONTAINER, typename LAMBDA>
-function filter(CONTAINER&& container, LAMBDA&& lambda) {
+def filter(CONTAINER&& container, LAMBDA&& lambda) {
     CONTAINER&& w{};
     std::copy_if(container.begin(), container.end(), std::back_inserter(w), lambda);
     return w;
 }
 
 template <typename T>
-function to_string(T t) {
+def to_string(T t) {
   return std::to_string(t);
 };
 
 template <typename T, typename U>
-function is_a(U u) {
+def is_a(U u) {
   return std::holds_alternative<T>(u);
 }
 
 template <typename T, typename U>
-function get_as(U u) {
+def get_as(U u) {
   return std::get<T>(u);
 }
 #pragma endregion
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,113 @@
+#pragma region includes
 #include "common.hpp"
-#include "cpprest/http_client.h"
+
+#include <algorithm>
+#include <exception>
+#include <iterator>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <variant>
+#include <vector>
+#include <memory>
+
+#include <cpprest/http_client.h>
+#include <cpprest/http_msg.h>
+#include <cpprest/ws_client.h>
+#include <cpprest/ws_msg.h>
+
+#include <rapidjson/document.h>
+#include <rapidjson/rapidjson.h>
+
+#include <fmt/core.h>
+
+#include <zmq.hpp>
+#pragma endregion
 
 #pragma region usings
 using except = std::exception;
 using std::runtime_error;
 using str = std::string;
-using std::variant;
 using std::vector;
-
-#define vec vector
-#define var variant
 
 using rapidjson::Document;
 using rapidjson::Value;
 
 auto &GET = web::http::methods::GET;
-auto &OK =  web::http::status_codes::OK;
+auto &OK = web::http::status_codes::OK;
 using web::http::client::http_client;
 using web::http::client::http_client_config;
 
+using web::websockets::client::websocket_callback_client;
+using web::websockets::client::websocket_incoming_message;
+
 using fmt::format;
 using fmt::print;
+
+using zmq::context_t;
+using zmq::socket_t;
 #pragma endregion
 
-function get_pairs_list(
-  const str& api_url,
-  const str& assets_path
-) -> var<vec<str>, except> begin
+#pragma region helpers
+#define vec vector
+#define var std::variant
+#define publisher zmq::socket_type::pub
+
+#pragma endregion
+
+#pragma region dsl
+#define lparen (
+#define rparen )
+#define lbrace {
+#define rbrace }
+#define if if lparen
+#define for for lparen
+#define then rparen lbrace
+#define doloop rparen lbrace
+#define endif rbrace
+#define endfor rbrace
+#define in :
+#define ne not_eq
+#pragma endregion
+struct tick_sub_req {
+  str event;
+  vec<str> pairs;
+  struct {
+    str name;
+  } subscription;
+};
+
+struct micro_service {
+  socket_t ticker_z;
+  websocket_callback_client ticker_ws;
+  context_t context_z = context_t();
+  bool is_running = false;
+  int tick_count = 0;
+  struct {
+    str zbind;
+    str ws_uri;
+  } config;
+};
+typedef std::shared_ptr<micro_service> MicroService;
+
+struct pair_price_update {
+  str trade_name;
+  double ask;
+  double bid;
+};
+
+let create_tick_sub_request = $(vec<str>& pairs) -> tick_sub_req {
+  return {
+    .event = "subscribe",
+    .pairs = pairs,
+    .subscription = {
+      .name = "ticker",
+    },
+  };
+};
+
+
+let get_pairs_list = $(str& api_url, str& assets_path) -> var<vec<str>, except> {
   // disable ssl configs for now
   mut config = http_client_config();
   config.set_validate_certificates(false);
@@ -35,9 +116,9 @@ function get_pairs_list(
   let response = http_client(api_url, config).request(GET, assets_path).get();
 
   // if not OK then return an error
-  if (response.status_code() not_eq OK) then
+  if response.status_code() not_eq OK then
     return runtime_error("Returned " + to_string(response.status_code()));
-  end
+  endif
 
   // extract the text from the response
   let response_text = response.extract_string().get();
@@ -45,27 +126,107 @@ function get_pairs_list(
   // attempt to parse the doc
   mut json_doc = Document();
   json_doc.Parse(response_text.c_str());
-  if (json_doc.HasParseError() or not json_doc.HasMember("result")) then
+  if json_doc.HasParseError() or not json_doc.HasMember("result") then
     return runtime_error(format("Failed to parse returned document: {}", response_text.c_str()));
-  end
+  endif
 
-  // extract wsname from the doc
+  // extract wsnames from the doc
   let obj = json_doc["result"].GetObject();
   mut pair_list = vec<str>();
-  for (let &pair in obj) begin
-    if (pair.value.HasMember("wsname")) then
+  for let& pair in obj doloop
+    if pair.value.HasMember("wsname") then
       pair_list.push_back(pair.value["wsname"].GetString());
-    end
-  end
+    endif
+  endfor
 
   return pair_list;
-end;
+};
 
-function main(i16 argc, c_str argv[]) -> i16 begin
+let is_ticker = $(Value& json) -> bool {
+  // validate the payload has the following fields
+  let required_members = {"a", "b", "c", "v", "p", "t", "l", "h", "o"};
+
+  // check each member in the requried members list
+  for let& memb in required_members doloop
+    // break if its missing any of them
+    if not json.HasMember(memb) then
+      return false;
+    endif
+  endfor
+
+  return true;
+};
+
+let parse_event = $(str& msg_data) -> var<pair_price_update, except> {
+  mut msg = Document();
+  msg.Parse(msg_data.c_str());
+
+  // validate the event parsed and there were not errors on the message itself
+  if msg.HasParseError() or msg.HasMember("errorMessage") then
+    return runtime_error(msg["errorMessage"].GetString());
+  endif
+
+  // validate it is a kraken publication type.  all kraken publications are arrays of size 4
+  if msg.Size() not_eq 4 then
+    return runtime_error("Not a publication");
+  endif
+
+  // get the payload of the publication
+  let payload = msg[1].GetObject();
+
+  // validate the payload is a tick object
+  if not is_ticker(payload) then
+    return runtime_error("Payload is not a tick");
+  endif
+
+  // get the pair name of the publication
+  let pair = msg[3].GetString();
+
+  // construct a neutral format for the price update event
+  return pair_price_update {
+    .trade_name = pair,
+    .ask = payload["a"][0].GetDouble(),
+    .bid = payload["b"][0].GetDouble(),
+  };
+};
+
+let process_tick = $(str& msg, MicroService ms) -> bool {
+  // short-circuit if shutting down
+  if not ms->is_running then
+    return false;
+  endif
+
+  // attempt to parse the event
+  let event = parse_event(msg);
+
+  // log then eat any errors
+  if is_a<except>(event) then
+    print(get_as<except>(event).what());
+    return false;
+  endif
+
+
+  // log healthy after 4 good ticks
+  ms->tick_count++;
+  if ms->tick_count == 4 then
+    print("Ticker is healthy.  Waiting on shutdown.");
+  endif
+
+  return true;
+};
+
+let tick_service = $(MicroService ms) -> i16 {
   let pairResults = get_pairs_list(API_URL, ASSETS_PATH);
-  if (is_a<except>(pairResults)) then
-    print("Failed to get instruments: {}\n", get_as<except>(pairResults).what());
-    return -1;
-  end
+  ms->ticker_z = socket_t(ms->context_z, publisher);
+  ms->ticker_z.bind(ms->config.zbind);
+  ms->ticker_ws.connect(ms->config.ws_uri).get();
+  ms->ticker_ws.set_message_handler([ms](websocket_incoming_message msg) {
+    process_tick(msg.extract_string().get(), ms);
+  });
   return 0;
-end
+};
+
+def main(i16 argc, c_str argv[]) -> i16 {
+  let ms = std::make_shared<micro_service>();
+  return tick_service(ms);
+}
