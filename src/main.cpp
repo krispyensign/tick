35ec057d47e5d8dfaf45c566e60b812041c18161
Jeremy Futral <jfutral@fanatics.com>
2021-02-23 17:06:46 -0500

    added backtrace handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
+#line __LINE__ "system.cpp"
+
+#include <execinfo.h>
+#include <getopt.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 #include "ticker_service.hpp"
 
-// TODO: add main argument parser
+// TODO: add main argument parser and options file
 // TODO: add sigint handler
-// TODO: add stacktracing
+
+#ifndef BACKTRACE_LENGTH
+#define BACKTRACE_LENGTH 20
+#endif
+
+auto backtrace_handler(int sig) -> void {
+  auto backtrace_array = array<void *, BACKTRACE_LENGTH>();
+  // get void*'s for all entries on the stack
+  auto size = backtrace(backtrace_array.data(), BACKTRACE_LENGTH);
+
+  // print out all the frames to stderr
+  std::cerr << "Error: signal " << sig << std::endl;
+  backtrace_symbols_fd(backtrace_array.data(), size, STDERR_FILENO);
+  exit(1);
+}
+
 
 auto main(i16 argc, c_str argv[]) -> i16 {
+  signal(SIGSEGV, backtrace_handler);
+  signal(SIGABRT, backtrace_handler);
+
   try {
     auto conf = service_config{
       .zbind = "tcp://*:9000",
