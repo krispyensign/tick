7c0fcf9d2292809bbb015bdf5acb67a38ef67990
Jeremy Futral <jfutral@fanatics.com>
2021-02-24 10:50:54 -0500

    fixed backward name

--- cmake/backwardcpp.cmake
+++ cmake/backwardcpp.cmake
@@ -1,8 +1,8 @@
 include(ExternalProject)
 
 ExternalProject_Add(
-  backward-cpp
-  PREFIX ${CMAKE_BINARY_DIR}/backward-cpp
+  backwardcpp
+  PREFIX ${CMAKE_BINARY_DIR}/backwardcpp
   GIT_REPOSITORY "https://github.com/bombela/backward-cpp"
   GIT_TAG master
   GIT_PROGRESS true
@@ -12,5 +12,5 @@ ExternalProject_Add(
   CMAKE_ARGS "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}"
 )
 
-ExternalProject_Get_Property(backward-cpp SOURCE_DIR)
+ExternalProject_Get_Property(backwardcpp SOURCE_DIR)
 set(BACKWARD_CPP_INCLUDE_DIR ${SOURCE_DIR}/ CACHE INTERNAL "Path to include backward-cpp")
