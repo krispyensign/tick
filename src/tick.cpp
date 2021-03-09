a270150fc5b869f6a6956569c1d7840021526b16
krispyensign <futralj@gmail.com>
2021-03-09 18:15:23 -0500

    Update tick.cpp

--- src/tick.cpp
+++ src/tick.cpp
@@ -27,7 +27,7 @@ def main(i16 argc, c_str argv[]) -> i16 {
   try {
     parser.ParseCLI(argc, argv);
   } catch (args::Help&) {
-    std::cout << parser;
+    fmt::print(parser);
     return 0;
   }
 
