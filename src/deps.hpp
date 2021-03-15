5c4699b4851c7f001ffcee927deed0c2083e0e32
KrispyEnsign <futralj@gmail.com>
2021-03-14 21:27:39 -0400

    fixed includes dir

--- src/deps.hpp
+++ src/deps.hpp
@@ -57,6 +57,27 @@ struct publisher {
   auto send(String msg) -> void { sock.send(message_t(msg), send_flags::none); };
 };
 
+struct pull {
+ private:
+  socket_t sock;
+
+ public:
+  pull() = delete;
+  pull(const Context& ctx, String bind_addr) {
+    sock = socket_t(*ctx, socket_type::pull);
+    sock.bind(bind_addr);
+  }
+  ~pull() { sock.close(); }
+  auto recv() -> optional<str> {
+    auto msg = message_t();
+    auto res = sock.recv(msg, zmq::recv_flags::none);
+    if (res != null) {
+      return msg.str();
+    }
+    return null;
+  }
+};
+
 struct websocket {
  private:
   websocket_callback_client client;
@@ -79,11 +100,16 @@ struct websocket {
 }  // namespace deps
 
 using Publisher = std::shared_ptr<deps::publisher>;
+using Pull = std::shared_ptr<deps::pull>;
 using WebSocket = std::shared_ptr<deps::websocket>;
 using Encoder = deps::Encoder;
 
 inline auto make_publisher(Context ctx, String bind_addr) -> Publisher {
   return make_shared<deps::publisher>(ctx, bind_addr);
+}
+
+inline auto make_pull(Context ctx, String bind_addr) -> Pull {
+  return make_shared<deps::pull>(ctx, bind_addr);
 }
 
 inline auto make_context(int threads) -> Context {
